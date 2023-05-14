88e8df610c016e9db2e00e67ef4472fdd48f5938
MRdevTagg <93151848+MRdevTagg@users.noreply.github.com>
2023-05-14 19:43:24 -0300

    Updated Animated class and added new methods
    create all Animated instances before you call them using the following methods:
    	NOTES: if you created the Animated instance using createMany() method, the names will be incremented with the
    	prefix 'your-svg-name, your-svg-name_1, your-svg-name_2, ...', corresponding to the order you created them
    
    	createOne() :
       create single Animated instance from id or class given to the object,
    	 @param id(string):  (mandatory) it must contain the id or className from the object you want to add to an Animated instance
    	 @param options(object): (optional) an object that must contain valid properties and values from the Animated class
    
    	createMany() :
       create many Animated instances from a className given to more than one object
    	 @param id(string): this argument is mandatory, it must contain className from the objects you want to add to an Animated instance
    	 @param options(object): this is an optional argument, an object that must contain valid properties and values from the Animated class
    
    	 */
    
    	/* Access Animations using the following methods:
    
    	getOne():
    	get a single Animated instance, it takes a string param that must be equal to the svg file name, if you created
    	the Animated instance using the createMany() method you must keep in mind the prefix increment explained in NOTES above
    
    	getAll() :
    	returns an object that contains all Animated instances created to this point, the keys are equal to the svg file name
    	with the prefix(if there is more than one with the same name),
    	you can destructure it and rename the Animated's references at your convenience
    
    	getArray() :
    	get an array from all Animated instances created to this point, this method has two optional params: filter and cb
    	@param filter(string): you can filter the Animated instances that contains this string in its svg file name
    	@param cb(function): you can set a callback function to execute for all animations, you must pass an argument to reference each Animated instances

--- SVGAlive/SVGAlive(JS).js
+++ SVGAlive/SVGAlive(JS).js
@@ -1,20 +1,63 @@
 /* eslint-disable  */
 // USE GUIDE:
-/*
- 1. - Give an id to the object that holds the svg like this: 
-        <object id="your-anim-id" data="../SVGAlive/your-animation-name.svg" type="image/svg+xml" ></object>;
- 2. - Now just pass the reference to the object in the object prop when you create the Animated instance
- 3. - Finally you will have access to all the methods and properties to animate your SVGAlive
- */
-import { Animated, getAll } from "./SVGAlive(controls).js";
-	window.addEventListener('load',()=>{
-	new Animated({
-		object : document.querySelector('#logo'),playmode:'ff'});
-	getAll().logo.fps = 35;	
-	getAll().logo.start();
 
-	document.querySelectorAll('.accordion-arrow').forEach(arr=>{
-		const newArrow = new Animated({object:arr,toggle:true,fps:60,playmode:'rew'})
-		newArrow.object.parentElement.addEventListener('click',()=>newArrow.start())
-	})
-})
+import {
+  createOne,
+  createMany,
+  getOne,
+  getAll,
+  getArray,
+} from "./SVGAlive(controls).js";
+window.addEventListener("load", () => {
+/* create all Animated instances before you call them using the following methods:
+	NOTES: if you created the Animated instance using createMany() method, the names will be incremented with the 
+	prefix 'your-svg-name, your-svg-name_1, your-svg-name_2, ...', corresponding to the order you created them
+
+	createOne() :
+   create single Animated instance from id or class given to the object,
+	 @param id(string):  (mandatory) it must contain the id or className from the object you want to add to an Animated instance
+	 @param options(object): (optional) an object that must contain valid properties and values from the Animated class
+
+	createMany() :
+   create many Animated instances from a className given to more than one object
+	 @param id(string): this argument is mandatory, it must contain className from the objects you want to add to an Animated instance
+	 @param options(object): this is an optional argument, an object that must contain valid properties and values from the Animated class
+	
+	 */
+
+	
+	/* Access Animations using the following methods:
+	
+	getOne():
+	get a single Animated instance, it takes a string param that must be equal to the svg file name, if you created 
+	the Animated instance using the createMany() method you must keep in mind the prefix increment explained in NOTES above
+  
+	getAll() : 
+	returns an object that contains all Animated instances created to this point, the keys are equal to the svg file name 
+	with the prefix(if there is more than one with the same name), 
+	you can destructure it and rename the Animated's references at your convenience
+	
+	getArray() :
+	get an array from all Animated instances created to this point, this method has two optional params: filter and cb
+	@param filter(string): you can filter the Animated instances that contains this string in its svg file name
+	@param cb(function): you can set a callback function to execute for all animations, you must pass an argument to reference each Animated instances
+	
+	*/
+  createOne("#logo",{ fps:35 });
+  createMany(".accordion_arrow", { fps: 60, playmode: "rew" });
+
+	getOne("mathiasRomeoLogo").start({});
+	getArray("arr2", (anim) =>
+	anim.object.parentElement.addEventListener("click", () =>
+	anim.start({ toggle: true, loop: false })
+	)
+  );
+
+	//const { mathiasRomeoLogo: logo, arr2_4: arrow4 } = getAll();
+	//logo.start({});
+  //getArray("arr2_", (anim) => (anim.fps = 12));
+
+  //arrow4.start({ loop: true });
+  //getOne("arr2_1").start({ loop: true });
+  //getArray("arr2")[3].start({ loop: true });
+});
--- SVGAlive/SVGAlive(controls).js
+++ SVGAlive/SVGAlive(controls).js
@@ -1,135 +1,145 @@
-
-export const animationsAll = {}
-
-export const getAll = () =>{
-	return animationsAll
-}
-export const createAnimated = (animation)=>{
-	let name = animation.object.id || animation.object.className;
-	for (const key in animationsAll) {
-			if (key === name) {
-				let prefix = 1;
-				while (animationsAll[name + "_" + prefix]) {
-					prefix++;
-				}
-				name += "_" + prefix;
-			};
-	};
-	animationsAll[name] = animation ;
-}
+export const animationsAll = {};
 export class Animated {
-	constructor({ object,min,max,fps,loopMode,playmode,loop,toggle })
-	{
-		this.object = object;
-		this.parent = object.contentDocument.querySelector(`svg`);
-		this.frames = [...this.parent.querySelectorAll('svg')];
-		this.fps = fps || 104;
-		this.loop = loop || false;
-		this.toggle = toggle || false;
-		this.min = (min >= 0 && min) || 0;
-		this.playmode = playmode || 'ff';
-		this.loopMode = loopMode || 'pingpong';
-		this.max = max <= this.frames.length-1 && max || this.frames.length-1; 
-		this.isPlaying = false;
-		this.direction = 0;
-		this.current = 0;
-		this.previous = null;
+  constructor({ object, min, max, fps, loopMode, playmode, loop, toggle }) {
+    this.object = object;
+    this.parent = object.contentDocument.querySelector(`svg`);
+    this.frames = [...this.parent.querySelectorAll("svg")];
+    this.fps = fps || 104;
+    this.loop = loop || false;
+    this.toggleOn = toggle || false;
+    this.min = (min >= 0 && min) || 0;
+    this.playmode = playmode || "ff";
+    this.loopMode = loopMode || "pingpong";
+    this.max = (max <= this.frames.length - 1 && max) || this.frames.length - 1;
+    this.isPlaying = false;
+    this.direction = 0;
+    this.current = 0;
+    this.previous = null;
 
-		this.condition = () => this.current <= this.max;
-		this.reachStart = () => this.current >= this.min;
-		this.reachEnd = () => this.current <= this.max;
-		this.reachPoint = (frame) => this.current === frame;
+    this.condition = () => this.current <= this.max;
+    this.reachStart = () => this.current >= this.min;
+    this.reachEnd = () => this.current <= this.max;
+    this.reachPoint = (frame) => this.current === frame;
 
-		this.animationFrameId = null;
-		this.lastFrameTime = null;
-		createAnimated(this)
-	}
+    this.frameID = null;
+    this.lastFrameTime = null;
+    createAnimated(this);
+  }
 
-	setPlaymode() {
-		switch (this.playmode) {
-			case 'ff':
-				this.direction = 1;
-				this.condition = () => this.reachEnd();
-				break;
-			case 'rew':
-				this.direction = -1;
-				this.condition = () => this.reachStart();
-				break;
-		}
-	}
+  setLoop() {
+     const mode = {
+      ff : () => (this.current = this.min),
+      pingpong : () => {
+        this.toggle()
+      },
+      rew : () => (this.current = this.max),
+    };
+		return mode[this.loopMode] && mode[this.loopMode]()
+  }
+  setPlay() {
+  const mode = {
+  "ff":()=>{
+        this.direction = 1;
+        this.condition = () => this.reachEnd();
+	},
+   "rew":()=>{
+        this.direction = -1;
+        this.condition = () => this.reachStart();
+	 }
+    }
+		return mode[this.playmode] && mode[this.playmode]()
+  }
 
-	draw(){
-		this.frames[this.previous]?.setAttribute('display', 'none');	
-    this.frames[this.current]?.setAttribute('display', 'initial');
-    this.previous = this.current;
-    this.current += this.direction;
-}
 
-	start() {
-		if(this.toggle){
-			this.previous = null;
-			if(this.playmode === 'ff'){
-				this.playmode = 'rew' 
-			}else{
+  start({toggle = null, loop = null} = null) {
+    loop && (this.loop = loop);
+    toggle && (this.toggleOn = toggle);
+    this.toggleOn && this.toggle();
+    if (!this.isPlaying) {
+      this.isPlaying = true;
+      
+      this.setInitialState();
+      this.draw();
+      this.lastFrameTime = performance.now();
+      this.animate();
+    }
+  }
+  
+  setInitialState() {
+    
+    this.loop && this.loopMode !== "pingpong" && (this.playmode = this.loopMode);
+    if (!this.loop && !this.toggleOn) {
+      this.frames[this.current].setAttribute("display", "none");
+      this.playmode === "ff" ? (this.current = this.min) : (this.current = this.max);
+    }
+  }
 
-				this.playmode = 'ff' 
-			}
-			 
-			}
-		if (!this.isPlaying) {
-			this.isPlaying = true;
-			this.loop && this.loopMode !== 'pingpong' && (this.playmode = this.loopMode)
-			if(!this.loop && !this.toggle){
-				this.frames[this.current].setAttribute('display','none')
-				this.playmode === 'ff'? this.current = this.min : this.current = this.max
-			 	}
-			this.draw()
-			
-			this.lastFrameTime = performance.now();
-			this.animate();
-		}
-	}
-
-	stop() {
-		cancelAnimationFrame(this.animationFrameId);
-		this.isPlaying = false;
-		this.lastFrameTime = null;
-	}
-
-	animate() {
+  animate() {
     if (!this.isPlaying) return;
-    const framesToDraw = Math.floor((performance.now() - this.lastFrameTime) / (1000 / this.fps));
- 
+    const framesToDraw = Math.floor(
+      (performance.now() - this.lastFrameTime) / (1000 / this.fps)
+    );
+
     for (let i = 0; i < framesToDraw; i++) {
-        this.lastFrameTime += 1000 / this.fps;
-        this.play();
+      this.lastFrameTime += 1000 / this.fps;
+      this.play();
     }
 
-    this.animationFrameId = requestAnimationFrame(this.animate.bind(this));
+    this.frameID = requestAnimationFrame(this.animate.bind(this));
+  }
+
+  play() {
+    this.setPlay();
+    this.condition() ? this.draw()
+    : this.loop ? this.setLoop()
+    : this.stop();
+  }
+
+  draw() {
+    this.frames[this.previous]?.setAttribute("display", "none");
+    this.frames[this.current]?.setAttribute("display", "initial");
+    this.previous = this.current;
+    this.current += this.direction;
+  }
+
+  stop() {
+    cancelAnimationFrame(this.frameID);
+    this.isPlaying = false;
+    this.lastFrameTime = null;
+  }
+  toggle(){
+    this.previous = null;
+    this.playmode === "ff" ? this.playmode = "rew" : this.playmode = "ff";
+   
+  }
+}
+const createAnimated = (animation) => {
+  let name = animation.object.getAttribute('data').split('/').pop().split('.').slice(0,-1).join('.').replace(/[^a-zA-Z0-9_]/g, '_');
+  for (const key in animationsAll) {
+    if (key === name) {
+      let prefix = 1;
+      while (animationsAll[name + "_" + prefix]) {
+        prefix++;
+      }
+      name += "_" + prefix;
+    }
+  }
+  animationsAll[name] = animation;
+};
+export const createOne = (idOrClassName,options) => new Animated({ object : document.querySelector(idOrClassName), ...options });
+export const createMany = (className,options) => 	document.querySelectorAll(className).forEach(anim=> new Animated({object:anim, ...options }));
+
+export const getOne = (id) => getAll()[id]
+export const getAll = () => animationsAll;
+export const getArray = (filter = null, cb) => {
+  const arrayToReturn = [];
+  for (const key in animationsAll) {
+    filter
+      ? key.includes(filter) && arrayToReturn.push(animationsAll[key])
+      : arrayToReturn.push(animationsAll[key]);
+  }
+  cb && arrayToReturn.map( anim => cb(anim))
+  return arrayToReturn;
 }
 
-	play() {
-		this.setPlaymode();
-		this.condition() ? this.draw() :
-		this.loop ? this.setLoopmode() :
-		this.stop();
-	}
 
-	setLoopmode() {
-		
-		switch (this.loopMode) {
-			case 'ff':
-				this.current = this.min;
-				break;
-			case 'pingpong':
-				this.previous = null
-				this.playmode === 'ff' ? this.playmode = 'rew' : this.playmode = 'ff';
-				break;
-			case 'rew':
-				this.current = this.max;
-				break;
-			default:
-				break;
-		}
-	}
-}
